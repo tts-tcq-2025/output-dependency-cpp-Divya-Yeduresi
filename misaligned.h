11e4659fef92a011640549ca9f761cf54300c29f
Divya-Yeduresi <Yeduresi.VenkataNagaDivyasree@bosch.com>
2025-08-16 12:11:37 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,1 +1,15 @@
-
+#ifndef COLOR_MAP_H
+#define COLOR_MAP_H
+ 
+#define MAJOR_COLORS 5
+#define MINOR_COLORS 5
+ 
+struct ColorPair {
+    int index;
+    const char* major;
+    const char* minor;
+};
+int printColourMap();
+ 
+ 
+#endif
